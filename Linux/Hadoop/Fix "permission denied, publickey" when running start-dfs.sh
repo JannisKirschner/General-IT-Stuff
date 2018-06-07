352497d8640945c5f908eae8f69a080649b3d674
Jannis <jannis.kirschner@gibmit.ch>
2018-06-07 16:18:06 +0200

    Create Fix "permission denied, publickey" when running start-dfs.sh

---
+++ Linux/Hadoop/Fix "permission denied, publickey" when running start-dfs.sh
@@ -0,0 +1,9 @@
+You have a problem with the ssh/permissions
+
+-First login as root
+-Then either create an ssh keypair or enable password based auth
+-Enable root per ssh
+
+(ssh localhost must work)
+
+Then it should be fixed
