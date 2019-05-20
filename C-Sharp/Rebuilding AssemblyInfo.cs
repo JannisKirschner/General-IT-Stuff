25225f404c46c4d7a48770712ee48d20eabf71cc
Jannis <jannis.kirschner@gibmit.ch>
2019-05-20 12:27:25 +0200

    Create Rebuilding AssemblyInfo.cs

---
+++ C-Sharp/Rebuilding AssemblyInfo.cs
@@ -0,0 +1,8 @@
+Within Visual Studio 2010, select Tools > Create GUID
+Click Copy (or New GUID then Copy)
+Click Exit
+Expand the Properties folder in the Solution Explorer (Control + W, S to open)
+Delete the AssemblyInfo.cs file shown with the exclaimation point
+Double click on the Properties folder - this should open the Properties window
+On the Application Tab, click the Application Information... button
+Fill in the application information, paste the GUID you copied into the GUID field - when done click OK
